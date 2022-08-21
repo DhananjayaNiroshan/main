92307f1d142ddbb8b412dc69a22d2aa007aa16ac
DhananjayaNiroshan <ar98384.fhss.sjp.ac.lk>
2022-08-22 00:29:44 +0530

    first commit

---
+++ App/root.js
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import {Image} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Profile from '../Screen/Profile';
+import Home from '../Screen/Home';
+import Discover from '../Screen/Discover';
+import Images from '../Screen/Images';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({route}) => ({title: ''})}>
+        <Tab.Screen
+          name="Home"
+          options={{
+            tabBarIcon: ({size = 25, focused}) => {
+              return (
+                <Image
+                  style={{width: size, height: size, focused: focused}}
+                  source={require('../Images/homeIcon.png')}
+                />
+              );
+            },
+          }}
+          component={Home}
+        />
+
+        <Tab.Screen
+          name="Discover"
+          options={{
+            tabBarIcon: ({size = 25, focused}) => {
+              return (
+                <Image
+                  style={{width: size, height: size, focused: focused}}
+                  source={require('../Images/search.png')}
+                />
+              );
+            },
+          }}
+          component={Discover}
+        />
+
+        <Tab.Screen
+          name="Images"
+          options={{
+            tabBarIcon: ({size = 25, focused}) => {
+              return (
+                <Image
+                  style={{width: size, height: size, focused: focused}}
+                  source={require('../Images/image.png')}
+                />
+              );
+            },
+          }}
+          component={Images}
+        />
+
+        <Tab.Screen
+          name="Profile"
+          options={{
+            tabBarIcon: ({size = 25, focused}) => {
+              return (
+                <Image
+                  style={{width: size, height: size, focused: focused}}
+                  source={require('../Images/profile.jpg')}
+                />
+              );
+            },
+          }}
+          component={Profile}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
